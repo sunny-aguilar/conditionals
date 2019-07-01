6b8060242acca0248ff804ebcae98d0355d20e5c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-30 22:29:19 -0700

    conditionals

--- template.asm
+++ template.asm
@@ -11,9 +11,9 @@ INCLUDE Irvine32.inc
 
 
 .data
-myQuote		BYTE	"Testing conditional statements", 0
-firstJump	BYTE	"Jumped to first label", 0
-secondJump	BYTE	"Jumped to second label", 0
+myQuote			BYTE	"Testing conditional statements", 0
+firstString		BYTE	"Jumped to first label", 0
+secondString	BYTE	"Jumped to second label", 0
 
 .code
 main PROC
@@ -28,14 +28,15 @@ main PROC
 
 ;a label to jump to
 	firstJump:
-	mov		edx, firstJump
-	call	SriteString
-	call	CrLf
+		mov		edx, firstString
+		call	SriteString
+		call	CrLf
 
 ;another label to jump to
-	secondJump:
-	call	WriteString
-	call	CrLf
+secondJump:
+		mov		eax, secondString
+		call	WriteString
+		call	CrLf
 
 	exit							; exit to operating system
 main ENDP
