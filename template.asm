0ee7fc2c32bde32e683c6aca32de4cdb8264df45
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-30 22:27:30 -0700

    conditionals

--- template.asm
+++ template.asm
@@ -12,6 +12,8 @@ INCLUDE Irvine32.inc
 
 .data
 myQuote		BYTE	"Testing conditional statements", 0
+firstJump	BYTE	"Jumped to first label", 0
+secondJump	BYTE	"Jumped to second label", 0
 
 .code
 main PROC
@@ -26,10 +28,10 @@ main PROC
 
 ;a label to jump to
 	firstJump:
-	
+		
 
 ;another label to jump to
-
+	secondJump:
 
 
 	exit							; exit to operating system
