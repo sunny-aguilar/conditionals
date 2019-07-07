ad9a97824841c699a767b95fbda51d6b4c07997d
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-07-07 13:03:41 -0700

    contionals

--- template.asm
+++ template.asm
@@ -11,95 +11,24 @@ INCLUDE Irvine32.inc
 
 
 .data
-caption			BYTE	"Dialog Title", 0
-myQuote			BYTE	"Testing conditional statements", 0
-infinity		BYTE	"INFINITE LOOP!", 0
-message			BYTE	"String in sub procedure", 0
-array			DWORD	1, 2, 3, 4, 5
-theSum			DWORD	?
-filename		BYTE	"newfile.txt", 0
-startTime		DWORD	?
-totalTime		BYTE	"total time: ", 0
-color			BYTE	?
-char			BYTE	?
+dontPrint		BYTE	"This should not print. . .", 0
+testString_1	BYTE	"first string", 0
+testString_2	BYTE	"secon dstring", 0
 
 .code
 main PROC
-	mov		esi, OFFSET array
-	mov		ecx, LENGTHOF array
-	call	arraySum
-	mov		theSum, eax
+	mov	eax, 0
+	mov	ebx, 0
+	push 5
+	push 10
+	pop ebx
+	pop eax
 	call	WriteDec
-	call	CrLf
-
-
-;looping example
-	mov		ax, 0
-	mov		ecx, 5
-
-L1:
-	call	WriteDec
-	call	CrLf
-	inc		ax
-	loop	L1
-
-call	WaitMsg				;pause program
-;call	Clrscr				;clear the screen
-;call	MsgBoxAsk			;popup box
-
-call	GetMseconds
-mov		startTime, eax
-
-;mov	edx, OFFSET filename
-;call	CreateOutputFile
-;call	CrLf
-
-mov		edx, OFFSET totalTime
-call	WriteString
-call	GetMseconds
-sub		eax, startTime
-call	WriteDec
-call	CrLf
-
-
-;get color of text and foreground
-call	GetTextColor
-mov		color, AL
-
-;message box
-;mov		ebx, OFFSET caption
-;mov		edx, OFFSET	myQuote
-;call	MsgBox
-
-;read a character
-call	ReadChar
-mov		char, al
 
 
 
-	exit; exit to operating system
+	exit								;exit to operating system
 main ENDP
 
-
-;------------------------------------------------
-; arraySum
-;
-; Adds array values to a sum
-;
-; Returns: none
-;------------------------------------------------
-arraySum PROC
-	push	esi
-	push	ecx
-	mov		eax, 0
-
-L1:
-	add		eax, [esi]
-	add		esi, TYPE DWORD
-	loop	L1
-	pop		ecx
-	pop		esi
-	ret
-arraySum ENDP
 
 END main
