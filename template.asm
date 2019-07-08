3d3e460de452c91d6d9028ba5c01c1af4a895bb6
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-07-07 19:37:34 -0700

    contionals

--- template.asm
+++ template.asm
@@ -2,7 +2,7 @@ TITLE Program Template     (template.asm)
 
 ; Author:					Sandro Aguilar
 ; Last Modified:			July 7, 2019
-; OSU email address:		aguilasa@or
+; OSU email address:		aguilasa@ore
 ; Course number/section:
 ; Assignment Number:
 ; Due Date:					N/A
