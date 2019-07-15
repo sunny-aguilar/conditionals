2cd31bbb728c1d0f83ba7e400fec30a45b010435
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-07-14 22:57:32 -0700

    conditionals

--- template.asm
+++ template.asm
@@ -4,7 +4,7 @@ TITLE Program Template     (template.asm)
 ; Last Modified:			July 7, 2019
 ; OSU email address:		aguilasa@oregonstate.edu
 ; Course number/section:
-; Assignment Number:		Sandro Ag
+; Assignment Number:		Sandro A
 ; Due Date:					N/A
 ; Description:
 
